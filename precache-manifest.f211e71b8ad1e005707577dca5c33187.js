930f53f0f582e7efb36fb54da4a1d587854da760
abdymalikmulky <abdymalikmulky@gmail.com>
2019-11-08 08:29:28 +0700

    Updates

---
+++ precache-manifest.f211e71b8ad1e005707577dca5c33187.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba2b81de4120309fc18d4e621304773c",
+    "url": "/react-hook-dota2wiki/index.html"
+  },
+  {
+    "revision": "017d50d87005c0a44e6f",
+    "url": "/react-hook-dota2wiki/static/css/main.301ebe38.chunk.css"
+  },
+  {
+    "revision": "889163dd6624cfb44721",
+    "url": "/react-hook-dota2wiki/static/js/2.859412ec.chunk.js"
+  },
+  {
+    "revision": "017d50d87005c0a44e6f",
+    "url": "/react-hook-dota2wiki/static/js/main.72a5b3f2.chunk.js"
+  },
+  {
+    "revision": "a40815a2b8a1aaf80db9",
+    "url": "/react-hook-dota2wiki/static/js/runtime-main.b5b08a78.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-hook-dota2wiki/static/media/logo.25bf045c.svg"
+  }
+]);
